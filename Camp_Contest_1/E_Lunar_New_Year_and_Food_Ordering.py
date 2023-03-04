22ebed483379fe9227e35beedc4d35996b327e7f
samuel <samuelkifle199314@gmail.com>
2023-03-04 08:38:53 +0300

    updated

---
+++ Camp_Contest_1/E_Lunar_New_Year_and_Food_Ordering.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+dishes = list(map(int, input().split()))
+costs = list(map(int, input().split()))
+
+customers = []
+
+for i in range(m):
+    customers.append(list(map(int, input().split())))
+
+for i in range(1000000):
+    print("Don't Laugh")
